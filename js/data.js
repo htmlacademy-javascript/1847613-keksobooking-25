ba507b1c230d833d2e26dab5725a212eb1ffee63
ArtemA <ar.ar7emy@yandex.ru>
2022-03-18 11:37:38 +0300

    Исправляет валидацию цены и меняет логику валидации комнат и гостей

--- js/data.js
+++ js/data.js
@@ -5,18 +5,12 @@ const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const TITLES = ['Title 1','Title 2', 'Title 3', 'Title 4', 'Title 5', 'Title 6', 'Title 7', 'Title 8'];
 const DESCRIPTIONS = ['Descr 1','Descr 2', 'Descr 3', 'Descr 4', 'Descr 5'];
-const TYPES_OF_INHABITATION = {
+const typesOfInhabitation = {
   'flat': 'Квартира',
   'bungalow': 'Бунгало',
   'house': 'Дом',
   'palace': 'Дворец',
   'hotel': 'Отель'
-};
-const CAPACITY_OPTION = {
-  '1': ['1'],
-  '2': ['1', '2'],
-  '3': ['1', '2', '3'],
-  '100': ['0']
 };
 const PRICE_OF_TYPES = {
   'bungalow': 0,
@@ -27,4 +21,4 @@ const PRICE_OF_TYPES = {
 };
 
 export {CHECKIN_TIMES, CHECKOUT_TIMES, PHOTOS, TYPES, FEATURES, TITLES,
-  DESCRIPTIONS, TYPES_OF_INHABITATION, CAPACITY_OPTION, PRICE_OF_TYPES};
+  DESCRIPTIONS, typesOfInhabitation, PRICE_OF_TYPES};
